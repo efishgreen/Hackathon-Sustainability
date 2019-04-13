15db72ff49bd7aa0d4b354f9f432a020ca029402
Anh B. Tran <anhb.tran1010@gmail.com>
2019-04-12 23:12:34 -0500

    Connect DB to SQL server

--- src/DBController.java
+++ src/DBController.java
@@ -1,13 +1,36 @@
 //import dblibrary.project.csci230.*;
 import java.util.*;
+import java.sql.*;
 
 
 public class DBController {
 	// ================================= INSTANCE VARIABLES =================================
-	//private UniversityDBLibrary ud;
+	private Connection connect;
+	private Statement st;
+	private ResultSet rs;
+	
 	// ================================= CONSTRUCTORS =================================
 	public DBController() {
 	    //this.ud = new UniversityDBLibrary("cloud9", "csci230");
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connect = DriverManager.getConnection("jdbc:mysql://fdb25.awardspace.net/3018019_efishdb"+"user=3018019_eFishDB&password=eFish@CSBSJU19");
+			st = connect.createStatement();
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	public void getData() {
+		try {
+			String query = "select * from 'Control Table'";
+			rs = st.executeQuery(query);
+			while(rs.next()) {
+				String  name = "";
+			}
+		}catch(Exception e) {
+			System.out.println(e);
+		}
 	}
 	// ================================= METHODS =================================
 	public ArrayList<ControlVariable> getAllControlVariables() {
