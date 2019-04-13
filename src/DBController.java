7bc21f9229576a4c27fcc3efdc72738caf106fdb
SLAUFERS001 <slaufers001@vplincsci011.ad.csbsju.edu>
2019-04-12 21:48:12 -0500

    WorkBuilder set up

--- src/DBController.java
+++ src/DBController.java
@@ -3,12 +3,13 @@ import java.util.*;
 
 
 public class DBController {
+	// ================================= INSTANCE VARIABLES =================================
 	//private UniversityDBLibrary ud;
-	
+	// ================================= CONSTRUCTORS =================================
 	public DBController() {
 	    //this.ud = new UniversityDBLibrary("cloud9", "csci230");
 	}
-
+	// ================================= METHODS =================================
 	public ArrayList<ControlVariable> getAllControlVariables() {
 		ArrayList<ControlVariable> contVarList = new ArrayList<ControlVariable>();
 		
--- src/EFishGreen.java
+++ src/EFishGreen.java
@@ -1,8 +1,62 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class EFishGreen {
 
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					EFishGreen window = new EFishGreen();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
 	public EFishGreen() {
-		// TODO Auto-generated constructor stub
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.SOUTH);
+		
+		JButton btnNewButton = new JButton("New button");
+		panel.add(btnNewButton);
+		
+		JButton importData = new JButton("Import");
+		importData.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		panel.add(importData);
+		
+		JButton btnNewButton_2 = new JButton("New button");
+		panel.add(btnNewButton_2);
 	}
 
 }
